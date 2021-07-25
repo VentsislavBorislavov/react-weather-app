123beb0166cd04d23cb350692045d63c1895fd8a
Vencislav Borislavov <64546764+VentsislavBorislavov@users.noreply.github.com>
2021-07-25 23:33:59 +0300

    city info can be set by clicking option from autocomplete

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,33 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useState } from 'react';
-import { searchQuery } from '../weatherApi';
 import useAutocomplete from '../hooks/useAutocomplete.js';
+import { setCityInfo } from '../redux/slices/searchSlice';
 
 const Search = () => {
 	const [ city, setCity ] = useState('');
 	const autocomplete = useAutocomplete(city);
+	const dispatch = useDispatch();
+	const chooseCity = (city) => {
+		console.log(city);
+		const cityInfo = {
+			key: city.Key,
+			name: city.LocalizedName,
+			country: city.Country.LocalizedName,
+			administrativeArea: city.AdministrativeArea.LocalizedName
+		};
+
+		dispatch(setCityInfo(cityInfo));
+	};
 	return (
 		<div className="search">
-			<input type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder="Search City" />
+			<div className="search-box">
+				<i className="fas fa-search-location" />
+				<input type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder="Search City" />
+			</div>
 			{autocomplete &&
 				autocomplete.map((place) => (
-					<div key={place.Key}>
+					<div key={place.Key} onClick={chooseCity(place)}>
 						<span className="name">{place.LocalizedName}</span>
-						<br />
 						<span className="info">{`${place.AdministrativeArea.LocalizedName}, ${place.Country
 							.LocalizedName}`}</span>
 					</div>
