a2dbf453eb3b2523e56d7943dc8b5da87d69dce0
Vencislav Borislavov <64546764+VentsislavBorislavov@users.noreply.github.com>
2021-07-25 19:19:38 +0300

    data showing properly

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,6 +9,15 @@ const Search = () => {
 	return (
 		<div className="search">
 			<input type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder="Search City" />
+			{autocomplete &&
+				autocomplete.map((place) => (
+					<div key={place.Key}>
+						<span className="name">{place.LocalizedName}</span>
+						<br />
+						<span className="info">{`${place.AdministrativeArea.LocalizedName}, ${place.Country
+							.LocalizedName}`}</span>
+					</div>
+				))}
 		</div>
 	);
 };
--- src/weatherApi/index.js
+++ src/weatherApi/index.js
@@ -1,1 +1,2 @@
-export const apiKey = '';
+export const apiKey = 'tyMIV07W9Kf5oJfcJ1XXCfEAAqmpu8uw';
+export const searchQuery = `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=`;
