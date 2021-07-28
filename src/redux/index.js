42f225c474f05f5d4aa8e3f1ddd02eaea79190dc
Vencislav Borislavov <64546764+VentsislavBorislavov@users.noreply.github.com>
2021-07-28 16:57:30 +0300

    place to store weather info

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import searchReducer from './slices/searchSlice';
+import weatherReducer from './slices/weatherSlice';
 
 export default configureStore({
 	reducer: {
-		search: searchReducer
+		search: searchReducer,
+		weather: weatherReducer
 	}
 });
