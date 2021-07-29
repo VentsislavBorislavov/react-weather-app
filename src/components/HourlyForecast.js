469b9d966cd5f6cb94d6189c51d6c358784ce682
Vencislav Borislavov <64546764+VentsislavBorislavov@users.noreply.github.com>
2021-07-29 22:25:07 +0300

    finished styling hourly forecast

--- src/components/HourlyForecast.js
+++ src/components/HourlyForecast.js
@@ -1,17 +1,24 @@
 import { images } from '../data/icons';
+import { useRef } from 'react';
 
 const HourlyForecast = ({ hourlyForecast }) => {
 	return (
 		<section className="hourly-forecast">
-			<h2>Hourly Forecast</h2>
+			<h2>12 Hour Forecast</h2>
 			<div className="display-forecast">
+				<button className="scroll scroll-left">
+					<i className="fas fa-arrow-left" />
+				</button>
 				{hourlyForecast.map((fcast) => (
-					<div key={fcast.EpochDateTime}>
+					<div key={fcast.EpochDateTime} className="forecast">
 						<img src={images[`${fcast.WeatherIcon}-s.png`].default} alt="weather icon" />
 						<p className="hour">{hourString(fcast.EpochDateTime)}</p>
 						<p className="temperature">{Math.floor(fcast.Temperature.Value)}</p>
 					</div>
 				))}
+				<button className="scroll scroll-right">
+					<i className="fas fa-arrow-right" />
+				</button>
 			</div>
 		</section>
 	);
--- src/weatherApi/index.js
+++ src/weatherApi/index.js
@@ -1,4 +1,4 @@
-export const apiKey = 'TWSsN2pYMsTWJAC5RJziigiXPuoePQnJ';
+export const apiKey = 'I4pGlQnueYSOQ6YsVMl9ALgGjzMWlqmd';
 export const searchQuery = `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=`;
 export const weatherQuery = (cityKey) =>
 	`http://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${apiKey}`;
