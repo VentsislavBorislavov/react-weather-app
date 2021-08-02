217b4a764ff3f05c240b1caede285709691dd828
Vencislav Borislavov <64546764+VentsislavBorislavov@users.noreply.github.com>
2021-08-02 20:14:23 +0300

    sun pos animates

--- src/components/SunPosition.js
+++ src/components/SunPosition.js
@@ -1,25 +1,45 @@
-import { Fragment } from 'react';
+import { Fragment, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { getTime } from '../data/time';
+import { useIntersection } from 'react-use';
+import gsap from 'gsap';
+
 const SunPosition = () => {
 	const todayWeather = useSelector((state) => state.weather.todayWeather);
+	const sunPosRef = useRef(null);
+	const intersection = useIntersection(sunPosRef, {
+		root: null,
+		rootMargin: '0px',
+		threshold: 0.9
+	});
+
+	const rotateArch = (element, rotation) => {
+		gsap.to(element, {
+			transform: `rotate(${rotation.deg}deg)`,
+			duration: rotation.animDur,
+			ease: 'power1.out'
+		});
+	};
+
+	const revertRotateArch = (element) => {
+		gsap.to(element, {
+			transform: 'rotate(-135deg)',
+			duration: 0
+		});
+	};
+
+	intersection && intersection.intersectionRatio > 0.9
+		? rotateArch('.arches', calculateSunPosition(todayWeather.sun.rise, todayWeather.sun.set))
+		: revertRotateArch('.arches');
 
 	return (
 		<Fragment>
 			{Object.keys(todayWeather).length > 0 && (
-				<section className="sun-position">
+				<section className="sun-position" ref={sunPosRef}>
 					<h2>Sun Position</h2>
 					<div className="info">
 						<div className="container">
-							<div
-								className="arches"
-								style={{
-									transform: `rotate(${calculateSunPosition(
-										todayWeather.sun.rise,
-										todayWeather.sun.set
-									)}deg)`
-								}}
-							>
+							<div className="arches">
 								<i className="fas fa-sun sun" />
 							</div>
 						</div>
@@ -37,21 +57,23 @@ const SunPosition = () => {
 const calculateSunPosition = (start, end) => {
 	const archRange = 180;
 	const endOfArch = 45;
+	const duration = 1.5;
 	const date = new Date();
 	const startDate = new Date(start) / 1000;
 	const endDate = new Date(end) / 1000;
 	const current = date.getTime() / 1000;
 	if (current - startDate < 0 || endDate - current < 0) {
-		return endOfArch;
+		return { deg: archRange, animDur: duration };
 	}
 	const range = endDate - startDate;
 	const placeInRange = current - startDate;
 	const percentageOfRange = placeInRange / range;
 	const percentageToArch = archRange * percentageOfRange;
 	const thingToRemoveFromRange = endOfArch + percentageToArch;
-	const finalResultForCss = thingToRemoveFromRange - archRange;
+	const deg = thingToRemoveFromRange - archRange;
+	const animDur = duration * percentageOfRange;
 
-	return finalResultForCss;
+	return { deg, animDur };
 };
 
 export default SunPosition;
