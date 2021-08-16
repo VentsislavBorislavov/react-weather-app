907bd9573ff2663cf1d357cc2355cbc3977b9da8
Vencislav Borislavov <64546764+VentsislavBorislavov@users.noreply.github.com>
2021-08-17 00:16:04 +0300

    made the api requests https so the deployed version can use them

--- src/weatherApi/index.js
+++ src/weatherApi/index.js
@@ -1,10 +1,10 @@
-export const apiKey = ""; //Place your accuweather api key here
-export const searchQuery = `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=`;
+export const apiKey = "xJST2MZjQ68twPEOOo0alpJNHx0iUvGl"; //Place your accuweather api key here
+export const searchQuery = `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=`;
 export const weatherQuery = (cityKey) =>
-  `http://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${apiKey}`;
+  `https://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${apiKey}`;
 export const todayForecast = (cityKey) =>
-  `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${cityKey}?apikey=${apiKey}&details=true&metric=true`;
+  `https://dataservice.accuweather.com/forecasts/v1/daily/1day/${cityKey}?apikey=${apiKey}&details=true&metric=true`;
 export const hourlyForecastLink = (cityKey) =>
-  `http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${cityKey}?apikey=${apiKey}&metric=true`;
+  `https://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${cityKey}?apikey=${apiKey}&metric=true`;
 export const dailyForecastLink = (cityKey) =>
-  `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=${apiKey}&metric=true`;
+  `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=${apiKey}&metric=true`;
